f7ca6cbe7e6fd86baf2f9cea9824d1150e1a826f
Marko Radulovic <marko.radulovic04@gmail.com>
2025-06-07 15:41:01 +0200

    initial code

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ 
+      minHeight: '100vh', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      fontSize: '2rem', 
+      fontFamily: 'sans-serif' 
+    }}>
+      Currently under construction
+    </div>
+  )
+}
+
+export default App
