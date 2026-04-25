c64617922f069eabee8277a72bc69b7e5085cede
Marko Radulovic <marko.radulovic04@gmail.com>
2026-04-25 15:34:35 +0200

    build: wire Next.js eslint preset and ignore .next

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import tseslint from "typescript-eslint"
+import { FlatCompat } from "@eslint/eslintrc"
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname })
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: [".next", "node_modules", "out"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
     },
   },
+  ...compat.extends("next/core-web-vitals")
 )
